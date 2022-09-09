f55ca0ccf101db50b908efa81f9f143bd08ac3d0
Lint Action <lint-action@samuelmeuli.com>
2022-09-09 07:00:31 +0000

    Fix code style issues with Black

--- src/countryModel.py
+++ src/countryModel.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
 from dash import Dash, dcc, html, Output, Input  # pip install dash
-import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
+import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 
 
 """
@@ -13,6 +13,7 @@ Next steps:
 Generate single csv for inpout?
 Upload to heroku
 """
+
 
 def risk_creator(input_df):
     """
@@ -50,12 +51,13 @@ def risk_creator(input_df):
 
     return df
 
+
 def weight_parameter(series, slider_weight):
     if slider_weight == 0:
         series = 0
     else:
-        series = series*slider_weight
-    
+        series = series * slider_weight
+
     return series
     #### NEXT STPE IF SLIDER WEIGHT = 0, amke stuff = 0
 
@@ -64,7 +66,7 @@ def weight_parameter(series, slider_weight):
 Start main function
 """
 path = "../data/country_data_master.csv"
-df = pd.read_csv(path, index_col = 'alpha3')
+df = pd.read_csv(path, index_col="alpha3")
 df = risk_creator(df)
 ignore_columns_index = 3
 
@@ -72,55 +74,82 @@ ignore_columns_index = 3
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
 
-
-mytitle = dcc.Markdown(children='')
-mysubtitle = dcc.Markdown(children='')
+mytitle = dcc.Markdown(children="")
+mysubtitle = dcc.Markdown(children="")
 mygraph = dcc.Graph(figure={})
-dropdown = dcc.Dropdown(options=df.columns.values[2:],
-                        value='Risk',  # initial value displayed when page first loads
-                        clearable=False)
+dropdown = dcc.Dropdown(
+    options=df.columns.values[2:],
+    value="Risk",  # initial value displayed when page first loads
+    clearable=False,
+)
 ### Create slider components on a card
 controls = dbc.Card(
     [
-        dbc.Label("Adjust the weighting of each parameter on the final risk score with the sliders"),
-
+        dbc.Label(
+            "Adjust the weighting of each parameter on the final risk score with the sliders"
+        ),
         html.Div(
             [
                 dbc.Label("Bike Ownership %"),
-                dcc.Slider(0, 400, value=100, id="myslider1" , updatemode='drag',
-                tooltip={"placement": "bottom", "always_visible": True}
+                dcc.Slider(
+                    0,
+                    400,
+                    value=100,
+                    id="myslider1",
+                    updatemode="drag",
+                    tooltip={"placement": "bottom", "always_visible": True},
                 ),
             ]
         ),
         html.Div(
             [
-                dbc.Label(df.columns[ignore_columns_index+1]),
-                dcc.Slider(0, 400, value=100, id="myslider2" , updatemode='drag',
-                tooltip={"placement": "bottom", "always_visible": True}
+                dbc.Label(df.columns[ignore_columns_index + 1]),
+                dcc.Slider(
+                    0,
+                    400,
+                    value=100,
+                    id="myslider2",
+                    updatemode="drag",
+                    tooltip={"placement": "bottom", "always_visible": True},
                 ),
             ]
         ),
         html.Div(
             [
-                dbc.Label(df.columns[ignore_columns_index+2]),
-                dcc.Slider(0, 400, value=100, id="myslider3" , updatemode='drag',
-                tooltip={"placement": "bottom", "always_visible": True}
+                dbc.Label(df.columns[ignore_columns_index + 2]),
+                dcc.Slider(
+                    0,
+                    400,
+                    value=100,
+                    id="myslider3",
+                    updatemode="drag",
+                    tooltip={"placement": "bottom", "always_visible": True},
                 ),
             ]
         ),
         html.Div(
             [
-                dbc.Label(df.columns[ignore_columns_index+3]),
-                dcc.Slider(0, 400, value=100, id="myslider4" , updatemode='drag',
-                tooltip={"placement": "bottom", "always_visible": True}
+                dbc.Label(df.columns[ignore_columns_index + 3]),
+                dcc.Slider(
+                    0,
+                    400,
+                    value=100,
+                    id="myslider4",
+                    updatemode="drag",
+                    tooltip={"placement": "bottom", "always_visible": True},
                 ),
             ]
         ),
         html.Div(
             [
-                dbc.Label(df.columns[ignore_columns_index+4]),
-                dcc.Slider(0, 400, value=100, id="myslider5" , updatemode='drag',
-                tooltip={"placement": "bottom", "always_visible": True}
+                dbc.Label(df.columns[ignore_columns_index + 4]),
+                dcc.Slider(
+                    0,
+                    400,
+                    value=100,
+                    id="myslider5",
+                    updatemode="drag",
+                    tooltip={"placement": "bottom", "always_visible": True},
                 ),
             ]
         ),
@@ -130,87 +159,88 @@ controls = dbc.Card(
 
 
 # Customize your own Layout
-app.layout = dbc.Container([
-
-    dbc.Row([
-        dbc.Col([mytitle], width=6)
-    ], justify='center'),
-    
-    
-    dbc.Row([
-        dbc.Col([mysubtitle], width=6)
-    ], justify='center'),
-    
-    
-    dbc.Row(
-        [
-            dbc.Col(controls, md=3),
-            dbc.Col([mygraph], width=9),
-        ],
-        align="center",
-    ),
-
-    dbc.Row([
-        dbc.Col([dropdown], width=6)
-    ], justify='center'),
-            html.Hr(),
-
-
-
-], fluid=True)
+app.layout = dbc.Container(
+    [
+        dbc.Row([dbc.Col([mytitle], width=6)], justify="center"),
+        dbc.Row([dbc.Col([mysubtitle], width=6)], justify="center"),
+        dbc.Row(
+            [
+                dbc.Col(controls, md=3),
+                dbc.Col([mygraph], width=9),
+            ],
+            align="center",
+        ),
+        dbc.Row([dbc.Col([dropdown], width=6)], justify="center"),
+        html.Hr(),
+    ],
+    fluid=True,
+)
 
 # Callback allows components to interact
 @app.callback(
-    Output(mygraph, 'figure'),
-    Output(mytitle, 'children'),
-    Output(mysubtitle, 'children'),
-    Input(dropdown, 'value'),
-    Input("myslider1", 'value'),
-    Input("myslider2", 'value'),
-    Input("myslider3", 'value'),
-    Input("myslider4", 'value'),
-    Input("myslider5", 'value')
+    Output(mygraph, "figure"),
+    Output(mytitle, "children"),
+    Output(mysubtitle, "children"),
+    Input(dropdown, "value"),
+    Input("myslider1", "value"),
+    Input("myslider2", "value"),
+    Input("myslider3", "value"),
+    Input("myslider4", "value"),
+    Input("myslider5", "value"),
 )
-def update_graph(column_name,scaling1,scaling2,scaling3,scaling4,scaling5):  # function arguments come from the component property of the Input
+def update_graph(
+    column_name, scaling1, scaling2, scaling3, scaling4, scaling5
+):  # function arguments come from the component property of the Input
     ignore_columns_num = 9
-    dff = df.copy() # create copy of dataframe to apply weightings to before creating the risk matrix
-    slider_weights = [scaling1,scaling2,scaling3,scaling4,scaling5]
+    dff = (
+        df.copy()
+    )  # create copy of dataframe to apply weightings to before creating the risk matrix
+    slider_weights = [scaling1, scaling2, scaling3, scaling4, scaling5]
 
-    for param_index in range(0,5):
-        series = weight_parameter(df.iloc[:,param_index+ignore_columns_num],slider_weights[param_index])
-        dff.iloc[:,param_index+ignore_columns_num] = series
-
+    for param_index in range(0, 5):
+        series = weight_parameter(
+            df.iloc[:, param_index + ignore_columns_num], slider_weights[param_index]
+        )
+        dff.iloc[:, param_index + ignore_columns_num] = series
 
     ### Sum all of the weighted risk values in to the 'Risk' column of the dataframe
-    dff['Risk'] = dff.iloc[:,ignore_columns_num:ignore_columns_num+param_index+1].sum(axis='columns',skipna=False)
+    dff["Risk"] = dff.iloc[
+        :, ignore_columns_num : ignore_columns_num + param_index + 1
+    ].sum(axis="columns", skipna=False)
     ### Normalise for a percentage risk,
-    dff['Risk'] = dff['Risk'] / max(dff['Risk'].dropna()) *100
+    dff["Risk"] = dff["Risk"] / max(dff["Risk"].dropna()) * 100
 
-    nancount = dff["Risk"].isnull().sum() 
-    mysubtitle = f"there are {len(df.index)-nancount} countries from a total of {len(df.index)}"
+    nancount = dff["Risk"].isnull().sum()
+    mysubtitle = (
+        f"there are {len(df.index)-nancount} countries from a total of {len(df.index)}"
+    )
 
     # dff['RiskAbsolute'] = risk_series
     # dff['Risk'] = risk_series / max(risk_series.dropna()) *100
 
     # https://plotly.com/python/choropleth-maps/
-    fig = px.choropleth(data_frame=dff,
-                        locations=dff.index,
-                        height=600,
-                        color=column_name,
-                        hover_name="Entity",
-                        hover_data=[
-                            "PBO",
-                            "Terrain Ruggedness",
-                            "Urban %",
-                            "Urban Agg %",
-                            "RoadQuality"
-                        ],
-                        )
+    fig = px.choropleth(
+        data_frame=dff,
+        locations=dff.index,
+        height=600,
+        color=column_name,
+        hover_name="Entity",
+        hover_data=[
+            "PBO",
+            "Terrain Ruggedness",
+            "Urban %",
+            "Urban Agg %",
+            "RoadQuality",
+        ],
+    )
 
-
-    return fig, "# " +column_name, mysubtitle  # returned objects are assigned to the component property of the Output
+    return (
+        fig,
+        "# " + column_name,
+        mysubtitle,
+    )  # returned objects are assigned to the component property of the Output
 
 
 # Run app
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run_server(debug=True, port=8054)
--- src/dahs_test.py
+++ src/dahs_test.py
@@ -2,71 +2,78 @@
 # https://github.com/Coding-with-Adam/Dash-by-Plotly#execute-code-in-browser
 
 from dash import Dash, dcc, Output, Input  # pip install dash
-import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
+import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 import plotly.express as px
-import pandas as pd                        # pip install pandas
+import pandas as pd  # pip install pandas
 
 # incorporate data into app
 # Source - https://www.cdc.gov/nchs/pressroom/stats_of_the_states.htm
-df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Good_to_Know/Dash2.0/social_capital.csv")
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Good_to_Know/Dash2.0/social_capital.csv"
+)
 print(df.head())
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
-mytitle = dcc.Markdown(children='')
+mytitle = dcc.Markdown(children="")
 mygraph = dcc.Graph(figure={})
-myslider = dcc.Slider(0, 400,
-        value=100,
-        updatemode='drag',
-        tooltip={"placement": "bottom", "always_visible": True})
-dropdown = dcc.Dropdown(options=df.columns.values[2:],
-                        value='Cesarean Delivery Rate',  # initial value displayed when page first loads
-                        clearable=False)
+myslider = dcc.Slider(
+    0,
+    400,
+    value=100,
+    updatemode="drag",
+    tooltip={"placement": "bottom", "always_visible": True},
+)
+dropdown = dcc.Dropdown(
+    options=df.columns.values[2:],
+    value="Cesarean Delivery Rate",  # initial value displayed when page first loads
+    clearable=False,
+)
 
 # Customize your own Layout
-app.layout = dbc.Container([
-    
-    dbc.Row([
-        dbc.Col([mytitle], width=6)
-    ], justify='center'),
-    dbc.Row([
-        dbc.Col([mygraph], width=12)
-    ]),
-    dbc.Row([
-        dbc.Col([dropdown], width=6)
-    ], justify='center'),
-    dbc.Row([
-        dbc.Col([myslider], width=6)
-    ], justify='center'),
-], fluid=True)
+app.layout = dbc.Container(
+    [
+        dbc.Row([dbc.Col([mytitle], width=6)], justify="center"),
+        dbc.Row([dbc.Col([mygraph], width=12)]),
+        dbc.Row([dbc.Col([dropdown], width=6)], justify="center"),
+        dbc.Row([dbc.Col([myslider], width=6)], justify="center"),
+    ],
+    fluid=True,
+)
 
 # Callback allows components to interact
 @app.callback(
-    Output(mygraph, 'figure'),
-    Output(mytitle, 'children'),
-    Input(dropdown, 'value'),
-    Input(myslider, 'value')
+    Output(mygraph, "figure"),
+    Output(mytitle, "children"),
+    Input(dropdown, "value"),
+    Input(myslider, "value"),
 )
-def update_graph(column_name,scaling):  # function arguments come from the component property of the Input
+def update_graph(
+    column_name, scaling
+):  # function arguments come from the component property of the Input
     print(column_name)
     print(type(column_name))
     dff = df
-    
-    dff['Cesarean Delivery Rate'] = dff['Cesarean Delivery Rate']*scaling/100
-    
-    
-    # https://plotly.com/python/choropleth-maps/
-    fig = px.choropleth(data_frame=dff,
-                        locations='STATE',
-                        locationmode="USA-states",
-                        scope="usa",
-                        height=600,
-                        color=column_name,
-                        animation_frame='YEAR')
 
-    return fig, '# '+column_name  # returned objects are assigned to the component property of the Output
+    dff["Cesarean Delivery Rate"] = dff["Cesarean Delivery Rate"] * scaling / 100
+
+    # https://plotly.com/python/choropleth-maps/
+    fig = px.choropleth(
+        data_frame=dff,
+        locations="STATE",
+        locationmode="USA-states",
+        scope="usa",
+        height=600,
+        color=column_name,
+        animation_frame="YEAR",
+    )
+
+    return (
+        fig,
+        "# " + column_name,
+    )  # returned objects are assigned to the component property of the Output
 
 
 # Run app
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run_server(debug=True, port=8054)
