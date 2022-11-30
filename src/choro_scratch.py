173ab47cd04c1b9695caa599065604c353b5d5cc
Kevin Rassool <kevin.rassool@gmail.com>
2022-11-30 14:05:28 +1100

    included hand pump data

--- src/choro_scratch.py
+++ src/choro_scratch.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
 
+choro_df = pd.read_csv("../data/WHO SUMMARY WATER ISO.csv",  index_col="alpha3")
+choro_df_recent = choro_df.groupby(choro_df.index).first() # works because csv is already in ascending date order
 
 # choro_df = pd.read_csv("../data/WHO_Drinking_Estimates_ISO.csv",  index_col="alpha3")
 choro_df = pd.read_csv("../data/WHO_Household_Water_Data_ISO.csv", index_col="alpha3")
@@ -8,16 +10,13 @@ choro_df_recent = choro_df.groupby(
     choro_df.index
 ).last()  # works because csv is already in ascending date order
 
-# argentina annoyling doesn't have data in 2020, so use the 2015 data
-choro_df_recent.loc["ARG", :] = (
-    choro_df.loc["ARG"].groupby(choro_df.loc["ARG"].index).first().values[0]
-)  # surely a better way?
+choro_df_recent.to_csv("slimmed.csv")
 
 cols = [i for i in choro_df_recent.columns if i not in ["Country"]]
 for col in cols:
     choro_df_recent[col] = pd.to_numeric(choro_df_recent[col])
 
-choro_df_recent.isnull().sum()
+# choro_df_recent.isnull().sum() find number of nans in each column
 
 colour_col = 5
 
