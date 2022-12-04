c0b9919a7e91e6e445331b9e73403a3c132c0ce0
Kevin Rassool <kevin.rassool@gmail.com>
2022-12-05 10:50:16 +1100

    added extra choros, first pass at popaultion moved

--- scripts/process_fuzzy_countries.py
+++ scripts/process_fuzzy_countries.py
@@ -20,18 +20,45 @@ def add_alpha_codes(df, col):
     return df
 
 
+def add_alpha_codes_from_ISO(df, col):
+    """
+    adds a column of alpha3 codes to a dataframe with country name in column 'col'
+    uses fuzzy logic to match countries
+    """
+    input_countries = df[col]
+    countries = []
+    for input_country in input_countries:
+        try:
+            country = pycountry.countries.get(numeric=str(input_country).zfill(3))
+            alpha3 = country.alpha_3
+        except:
+            alpha3 = "unk_" + str(input_country)
+        countries.append(alpha3)
+    df["alpha3"] = countries
+    return df
+
+
+
+
+
+
+
 def read_road_values_fuzzy(path, col):
     df = pd.read_csv(path)
     df = add_alpha_codes(df, col)
     df = df.set_index("alpha3")
     return df
 
-
+iso_num_col = "ISO Code"
 col = "Country"
-path = "../data/hand_pump_data.csv"
+# path = "../data/hand_pump_data.csv"
+path =  "household_countries.csv"
+df_original = pd.read_csv(path)
 
-df = read_road_values_fuzzy(path, col)
 
 
-filename = "hand_pump_data"
+df = add_alpha_codes_from_ISO(df_original, iso_num_col)
+
+
+filename = "household_countries_ISO"
 df.to_csv(filename + ".csv")
