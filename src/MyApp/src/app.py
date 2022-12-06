45213cdcd4c689b7a2cefa9f0e4757fc6cd4e384
Kevin Rassool <kevin.rassool@gmail.com>
2022-12-06 16:36:47 +1100

    Deploy to Heroku for app allfed-water-access - dashtools

---
+++ src/MyApp/src/app.py
@@ -0,0 +1,38 @@
+'''
+ # @ Create Time: 2022-12-06 16:19:25.311961
+'''
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__, title="MyApp")
+
+# Declare server for Heroku deployment. Needed for Procfile.
+server = app.server
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ src/MyCSVApp/README.md
@@ -0,0 +1,11 @@
+# Dash CSV Template: MyCSVApp
+
+Created on 2022-12-06 16:25:19.116570
+
+Welcome to your [Plotly Dash](https://plotly.com/dash/) App! This is a template for your MyCSVApp app.
+
+See the [Dash Documentation](https://dash.plotly.com/introduction) for more information on how to get your app up and running.
+
+## Running the App
+
+Run `src/app.py` and navigate to http://127.0.0.1:8050/ in your browser.
