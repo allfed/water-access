dbd5a5d75189264efa0630c012ce17116b00cfde
Lint Action <lint-action@samuelmeuli.com>
2023-01-03 05:15:29 +0000

    Fix code style issues with Black

--- src/MyApp/src/app.py
+++ src/MyApp/src/app.py
@@ -1,6 +1,6 @@
-'''
+"""
  # @ Create Time: 2022-12-06 16:19:25.311961
-'''
+"""
 
 from dash import Dash, html, dcc
 import plotly.express as px
@@ -13,26 +13,27 @@ server = app.server
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.DataFrame(
+    {
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
+)
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash"),
+        html.Div(
+            children="""
         Dash: A web application framework for your data.
-    '''),
+    """
+        ),
+        dcc.Graph(id="example-graph", figure=fig),
+    ]
+)
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
--- src/scratch.py
+++ src/scratch.py
@@ -1,17 +1,15 @@
-
 import pathlib
 import pandas as pd
 import numpy as np
 
 
 def load_data(data_file: str) -> pd.DataFrame:
-    '''
+    """
     Load data from /data directory
-    '''
+    """
     PATH = pathlib.Path(__file__).parent
     DATA_PATH = PATH.joinpath("../data").resolve()
     return pd.read_csv(DATA_PATH.joinpath(data_file))
-
 
 
 df1 = load_data("rough_pass_kummu_short.csv")
@@ -24,5 +22,7 @@ df1 = load_data("rough_pass_kummu_short.csv")
 # df1["pop_density"] = df1["pop_density"] / df1["pop_density"].sum()
 
 # convert population density to percent of national population on a per country basis, grouped by ISO_CC
-df1["pop_density_perc"] = df1.groupby("ISO_CC")["pop_density1"].apply(lambda x: x / x.sum())
+df1["pop_density_perc"] = df1.groupby("ISO_CC")["pop_density1"].apply(
+    lambda x: x / x.sum()
+)
 # df1["population in region"] = df1["pop_density_perc"] * df1["population"]
